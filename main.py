7dc61110188b23e509bc5cf8fb52c7f65bdf6310
mubar3 <khusnul.mubar@yahoo.com>
2025-02-02 09:35:08 +0700

    update code

--- main.py
+++ main.py
@@ -10,9 +10,31 @@ def balik_value (value):
         karakter_terbalik = karakter_terbalik + angka
     return karakter_terbalik
 
-print(balik_value(credit_card_split[0]))
-print(balik_value(credit_card_split[1]))
 
+def hitung_value (value):
+    total=0
+    for angka in value:
+        total = total + int(angka)
+    return total
+
+def ubah_value_genap (value):
+    output=''
+    for index, angka in enumerate(value):
+        # cek bilangan genap
+        if (index + 1) % 2 == 0:
+            output = output + str(int(angka) * 2)  
+        else:
+            output = output + angka 
+    return output
+
+# print(balik_value(credit_card_split[0]))
+# print(ubah_value_genap( balik_value(credit_card_split[0]) ))
+print(ubah_value_genap( balik_value(credit_card_split[0]) ) 
+    + ' ' +
+     ubah_value_genap( balik_value(credit_card_split[1]) )
+)
+# print(hitung_value(credit_card_split[0]))
+# print( hitung_value(credit_card_split[0] + credit_card_split[1] + credit_card_split[2] + credit_card_split[3]) ) 
 
 
 # print(karakter_terbalik)
