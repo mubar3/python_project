be07d7908c1c5f6661c7c6be50f6ea5f9b317c0b
mubar3 <khusnul.mubar@yahoo.com>
2025-02-02 10:07:37 +0700

    tambah pengecekan

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 
-credit_card="2323-2005-7766-3554"
+credit_card=input('credit card anda : ')
+# credit_card="2323-2005-7766-3554"
 credit_card_split=credit_card.split('-')
+credit_card_number=credit_card.replace('-','')
 
-print(credit_card_split)
+# pengecekan lenght credit card
+if len(credit_card_number) > 16:
+    print('credit card tidak valid')
+    exit()
+
+for angka in credit_card_number:
+    try:
+        int(angka)
+    except ValueError:
+        print('credit card tidak valid')
+        exit()
 
 def balik_value (value):
     karakter_terbalik=''
@@ -27,14 +39,19 @@ def ubah_value_genap (value):
             output = output + angka 
     return output
 
-# print(balik_value(credit_card_split[0]))
-# print(ubah_value_genap( balik_value(credit_card_split[0]) ))
+print(
+    balik_value(credit_card_split[0])
+    + ' ' +
+    balik_value(credit_card_split[1])
+)
+
 print(ubah_value_genap( balik_value(credit_card_split[0]) ) 
     + ' ' +
      ubah_value_genap( balik_value(credit_card_split[1]) )
 )
-# print(hitung_value(credit_card_split[0]))
-# print( hitung_value(credit_card_split[0] + credit_card_split[1] + credit_card_split[2] + credit_card_split[3]) ) 
+
+print(hitung_value(credit_card_split[0]))
+print( hitung_value(credit_card_split[0] + credit_card_split[1] + credit_card_split[2] + credit_card_split[3]) ) 
 
 
 # print(karakter_terbalik)
